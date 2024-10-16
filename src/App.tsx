e305147c5d8f7c102db949c5ec641b5f344ec837
Souvik Das <souvikdas.dev@gmail.com>
2024-10-16 23:01:53 +0530

    Added AddTaskForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Dropdown from "./components/Dropdown";
 import Sidebar from "./Sidebar";
+import AddTaskForm from "./AddTaskForm";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -65,7 +66,7 @@ function App() {
               </button>
               <button
                 type="button"
-                className="p-2 ml-4 -m-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+                className="p-2 ml-4 -m-2 text-gray-700 hover:text-gray-900 sm:ml-6 lg:hidden"
               >
                 <span className="sr-only">Filters</span>
                 <svg
@@ -106,49 +107,7 @@ function App() {
             </div>
           </div>
 
-          <form className="relative w-full my-4" onSubmit={() => alert(1)}>
-            <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-5 text-gray-500"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="voice-search"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 font-medium ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-              placeholder="Add something to get done..."
-              required
-            />
-            <button
-              type="button"
-              className="absolute inset-y-0 flex items-center end-0 pe-3"
-            >
-              <svg
-                className="w-4 h-4 text-gray-500 hover:text-gray-900"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 16 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z"
-                />
-              </svg>
-            </button>
-          </form>
+          <AddTaskForm />
         </div>
       </div>
     </div>
